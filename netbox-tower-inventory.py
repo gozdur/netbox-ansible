44ed0fcb73b82e46115a86f58b0592c729d10dbf
gozdur <gozdur>
2020-08-21 01:39:40 +0200

    commit

---
+++ netbox-tower-inventory.py
@@ -0,0 +1,78 @@
+
+import json
+import requests
+
+# Netbox URL
+URL = 'http://35.198.136.252:32768/'
+
+# Netbox API Token
+TOKEN = '126262c5b9c13a7a1102ee1e1a416004f5bf8217'
+
+# AWX Filter Tags
+FILTER_TAGS = []
+
+headers = {
+    'Accept': 'application/json ; indent=4',
+    'Authorization': 'Token %s' % (TOKEN),
+}
+
+api_url = URL + '/api/dcim/devices/'
+hosts_list = []
+devices = []
+sites = {}
+racks = {}
+platforms = {}
+tenants = {}
+tags = {}
+inventory = {}
+hostvars = {}
+
+# Get data from netbox
+
+while api_url:
+    api_output = requests.get(api_url, headers=headers)
+    api_output_data = api_output.json()
+
+    if isinstance(api_output_data, dict) and "results" in api_output_data:
+        hosts_list += api_output_data["results"]
+        api_url = api_output_data["next"]
+
+# # Filter hosts for AWX
+
+# for i in hosts_list:
+#     if FILTER_TAGS:
+#         if any(item in FILTER_TAGS for item in i['tags']):
+#             devices.append(i)
+#     else:
+#         devices.append(i)
+
+# # Populate inventory
+
+# for i in devices:
+#     if i['name']:
+#         if i['config_context']:
+#             hostvars.setdefault('_meta', {'hostvars': {}})['hostvars'][i['name']] = i['config_context']
+#         if i['site']:
+#             sites.setdefault('site_' + i['site']['slug'], {'hosts': []})['hosts'].append(i['name'])
+#             hostvars.setdefault('_meta', {'hostvars': {}})['hostvars'][i['name']].setdefault('tags', {})['site'] = i['site']['slug']
+#         if i['rack']:
+#             racks.setdefault('rack_' + i['rack']['name'], {'hosts': []})['hosts'].append(i['name'])
+#             hostvars.setdefault('_meta', {'hostvars': {}})['hostvars'][i['name']].setdefault('tags', {})['rack'] = i['rack']['name']
+#         if i['platform']:
+#             platforms.setdefault('platform_' + i['platform']['slug'], {'hosts': []})['hosts'].append(i['name'])
+#             hostvars.setdefault('_meta', {'hostvars': {}})['hostvars'][i['name']].setdefault('tags', {})['platform'] = i['platform']['slug']
+#         if i['tenant']:
+#             tenants.setdefault('tenant_' + i['tenant']['slug'], {'hosts': []})['hosts'].append(i['name'])
+#             hostvars.setdefault('_meta', {'hostvars': {}})['hostvars'][i['name']].setdefault('tags', {})['tenant'] = i['tenant']['slug']
+#         for t in i['tags']:
+#             tags.setdefault(t, {'hosts': []})['hosts'].append(i['name'])
+#             hostvars.setdefault('_meta', {'hostvars': {}})['hostvars'][i['name']].setdefault('tags', {})[t] = True
+
+# inventory.update(sites)
+# inventory.update(racks)
+# inventory.update(platforms)
+# inventory.update(tenants)
+# inventory.update(tags)
+# inventory.update(hostvars)
+
+# print(json.dumps(inventory, indent=4))
---
+++ pg_inventory.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import json
+import requests
+from pprint import pprint
+
+# Netbox URL
+URL = 'http://35.198.136.252:32768'
+
+# Netbox API Token
+TOKEN = '126262c5b9c13a7a1102ee1e1a416004f5bf8217'
+
+headers = {  
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': 'Token ' + TOKEN
+}
+# Netbox app's
+
+DEVICES = '/api/dcim/devices'
+
+
+
+def get_dcim_devices():
+    dcim_devices = requests.get(URL + DEVICES, headers = headers).json()
+    return dcim_devices
+
+
+if __name__ == "__main__":
+
+    devices = []
+    inventory = {}
+    hostvars = {}
+    allh = {}
+
+    output = get_dcim_devices()
+    if isinstance(output, dict) and "results" in output:
+        devices += output['results']
+
+        for i in devices:
+            if i['primary_ip']:
+                hostvars.setdefault('_meta', {'hostvars': {}})['hostvars'][i['name']] = {"ansible_host": str(i['primary_ip']['address']).split("/", 1)[0]}
+            if i['config_context']:
+                hostvars['_meta']['hostvars'][i['name']].update(i['config_context'])
+        for i in devices:
+            if i ['name']:
+                allh.setdefault('all', {'hosts': []})['hosts'].append(i['name'])
+
+inventory.update(hostvars)
+inventory.update(allh)
+
+print(json.dumps(inventory, indent=4))
